d547f05a4fbc596b171d71363b1fddd838a5903b
azrael8576 <azrael8576love2008@gmail.com>
2018-11-25 17:40:08 +0800

    Day 14 - RecyclerView（2）

--- app/src/main/java/com/alex/myapplication2/MainActivity.java
+++ app/src/main/java/com/alex/myapplication2/MainActivity.java
@@ -3,11 +3,13 @@ package com.alex.myapplication2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -30,12 +32,26 @@ public class MainActivity extends AppCompatActivity {
         // 連結元件
         recycler_view = findViewById(R.id.recycler_view);
         // 設置RecyclerView為列表型態
-        recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        // Linear型態，第二個參數控制垂直或水平，第三個參數為是否reverse順序
+        recycler_view.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        // Grid型態，第二個參數控制一列顯示幾項
+//        recycler_view.setLayoutManager(new GridLayoutManager(this, 2));
         // 設置格線
-        recycler_view.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+        // 第二個參數VERTICAL或HORIZONTAL控制垂直或水平
+        recycler_view.addItemDecoration(
+                new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
         // 將資料交給adapter
         adapter = new MyAdapter(mData);
         // 設置adapter給recycler_view
         recycler_view.setAdapter(adapter);
+
+        Button btnAdd = findViewById(R.id.btnAdd);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 新增一個項目
+                adapter.addItem("New Item");
+            }
+        });
     }
 }
--- app/src/main/java/com/alex/myapplication2/MyAdapter.java
+++ app/src/main/java/com/alex/myapplication2/MyAdapter.java
@@ -5,7 +5,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -37,10 +39,40 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     // 建立ViewHolder
     class ViewHolder extends RecyclerView.ViewHolder{
+        // 宣告元件
         private TextView txtItem;
+        private Button btnRemove;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtItem = itemView.findViewById(R.id.txtItem);
+            btnRemove = itemView.findViewById(R.id.btnRemove);
+
+            // 點擊項目時
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(view.getContext(),"click "+getAdapterPosition(),Toast.LENGTH_LONG).show();
+                }
+            });
+            // 點擊項目中的Button時
+            btnRemove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 移除項目，getAdapterPosition為點擊的項目位置
+                    removeItem(getAdapterPosition());
+                }
+            });
         }
+    }
+    //新增項目
+    public void addItem(String text){
+        // 為了示範效果，固定新增在位置3。若要新增在最前面就把3改成0
+        mData.add(3,text);
+        notifyItemInserted(3);
+    }
+    // 刪除項目
+    public void removeItem(int position){
+        mData.remove(position);
+        notifyItemRemoved(position);
     }
 }
